7466464b029e9efcbeb35f1547da7bd28eb2f9ee
Claudio Bley <claudio.bley@gmail.com>
2020-11-03 10:02:32 +0100

    Add needed build inputs and use adoptopenjdk

--- shell.nix
+++ shell.nix
@@ -2,6 +2,9 @@
 }:
 
 project.pkgs.mkShell {
+  CLANG_PATH = project.clang + "/bin/clang";
+  CLANGPP_PATH = project.clang + "/bin/clang++";
+
   buildInputs = builtins.attrValues project.devTools;
   shellHook = ''
     ${project.ci.pre-commit-check.shellHook}
