9dc053cb5fe2fed5066524bac9528f0c5f3f34c9
Claudio Bley <claudio.bley@gmail.com>
2021-04-25 14:34:11 +0200

    Build scala-native with nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 { project ? import ./nix { }
 }:
-
+let
+  scala-native = import ./scala-native.nix { inherit project; };
+in
 project.pkgs.mkShell {
   CLANG_PATH = project.clang + "/bin/clang";
   CLANGPP_PATH = project.clang + "/bin/clang++";
@@ -8,5 +10,8 @@ project.pkgs.mkShell {
   buildInputs = builtins.attrValues project.devTools;
   shellHook = ''
     ${project.ci.pre-commit-check.shellHook}
+    mkdir --parents "$HOME/.ivy2/local"
+    ln --symbolic --force --target-directory="$HOME/.ivy2/local" "${scala-native.deps}/org.scala-native"
   '';
+
 }
