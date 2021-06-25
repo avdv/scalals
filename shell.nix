9e84490227aec6a81f4fb90edf3849f3d6ab85e5
Claudio Bley <cbley@exa-online.de>
2021-06-25 16:00:48 +0200

    Build a static binary with musl

--- shell.nix
+++ shell.nix
@@ -2,15 +2,17 @@
 }:
 let
   scala-native = import ./scala-native.nix { inherit project; };
-  mkShell = project.pkgs.mkShell.override { stdenv = project.pkgs.stdenvNoCC; };
+  pkgs = project.pkgs.pkgsStatic;
+  stdenv = pkgs.llvmPackages_11.libcxxStdenv;
+  mkShell = pkgs.mkShell.override { inherit stdenv; };
 in
 mkShell {
-  CLANG_PATH = project.clang + "/bin/clang";
-  CLANGPP_PATH = project.clang + "/bin/clang++";
-
   nativeBuildInputs = builtins.attrValues project.devTools;
 
   shellHook = ''
+    export CLANG_PATH="$NIX_CC/bin/$CC"
+    export CLANGPP_PATH="$NIX_CC/bin/$CXX"
+
     ${project.ci.pre-commit-check.shellHook}
     mkdir --parents "$HOME/.ivy2/local"
     ln --symbolic --force --target-directory="$HOME/.ivy2/local" "${scala-native.deps}/org.scala-native"
