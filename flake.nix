9001e42fa97687fef242b568eb6951f8b10cdea8
Scala Steward <me@scala-steward.org>
2021-12-06 22:09:56 +0100

    Update auxlib, javalib, nativelib, nscplugin, ... to 0.4.2

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
             pname = "scalals-native";
             version = "0.1.3";
 
-            depsSha256 = "sha256:05f6j8mcxpk7wal9l7c4d8c8ijfwzq2zrzj0w444svmg6jfcfpc7";
+            depsSha256 = "sha256-qNYgIebUXHGExIWQEIyezhEnEs0e5eLjMhgFLWS95pI=";
 
             src = ./.;
 
