b3304aa09dc9b623ec43d9425bb8e6ae49ea1cde
Claudio Bley <claudio.bley@gmail.com>
2022-03-21 22:58:20 +0100

    Reformat with scalafmt 3.4.3

--- shared/src/main/scala/de/bley/scalals/Core.scala
+++ shared/src/main/scala/de/bley/scalals/Core.scala
@@ -83,11 +83,12 @@ trait Core {
     val listingBuffer = scala.collection.mutable.TreeSet.empty[generic.FileInfo]
     for {
       path <- items
-    } try {
-      listingBuffer += FileInfo(path, config.dereference)
-    } catch {
-      case e: IOException => Console.err.println(s"scalals: cannot access '$path': ${e.getMessage}")
     }
+      try {
+        listingBuffer += FileInfo(path, config.dereference)
+      } catch {
+        case e: IOException => Console.err.println(s"scalals: cannot access '$path': ${e.getMessage}")
+      }
     listingBuffer
   }
 
@@ -107,16 +108,16 @@ trait Core {
       config: Config,
       decorators: Vector[Decorator]
   ): Unit = {
-    //import java.util.{ List => JList }
-    //import java.util.function.Supplier
-    //scala.collection.mutable.ArrayBuffer.empty[FileInfo]
+    // import java.util.{ List => JList }
+    // import java.util.function.Supplier
+    // scala.collection.mutable.ArrayBuffer.empty[FileInfo]
 
-    //val supplier: Supplier[JList[FileInfo]] = () => listingBuffer.asJava
-    //new ArrayList[FileInfo](100)
+    // val supplier: Supplier[JList[FileInfo]] = () => listingBuffer.asJava
+    // new ArrayList[FileInfo](100)
 //    timing("collect"){
 
-    //.collect(Collectors.toCollection[FileInfo, JList[FileInfo]](supplier))
-    //Collectors.toList())
+    // .collect(Collectors.toCollection[FileInfo, JList[FileInfo]](supplier))
+    // Collectors.toList())
 
 //    timing("sort")(listing.sort(if (config.reverse) comparator.reversed() else comparator))
 //    val sorted = timing("sort")(listingBuffer.sortWith(if (config.reverse) { (a: FileInfo, b: FileInfo) => !comparator(a, b) } else comparator))
@@ -132,7 +133,7 @@ trait Core {
         }
 
       val sizes = output.map(_._1)
-      //val minlen = sizes.min
+      // val minlen = sizes.min
       val columns =
         if (config.long || config.oneLine) decorators.size
         else {
@@ -149,8 +150,8 @@ trait Core {
         h.transpose.map(_.max)
       }
 
-      //Console.err.println(s"$columns")
-      //Console.err.println(maxColSize.mkString(", "))
+      // Console.err.println(s"$columns")
+      // Console.err.println(maxColSize.mkString(", "))
       for {
         record <- output.grouped(columns)
       } {
@@ -170,13 +171,13 @@ trait Core {
     }
   }
 
-  //lazy val dateFormat = DateTimeFormatter.ofPattern("MMM ppd  yyyy", Locale.getDefault())
-  //val dateFormat = new SimpleDateFormat() //.ofPattern("MMM ppd  yyyy", Locale.getDefault())
-  //lazy val recentFormat = DateTimeFormatter.ofPattern("MMM ppd HH:mm", Locale.getDefault())
-  //lazy val currentZone = ZoneId.systemDefault()
+  // lazy val dateFormat = DateTimeFormatter.ofPattern("MMM ppd  yyyy", Locale.getDefault())
+  // val dateFormat = new SimpleDateFormat() //.ofPattern("MMM ppd  yyyy", Locale.getDefault())
+  // lazy val recentFormat = DateTimeFormatter.ofPattern("MMM ppd HH:mm", Locale.getDefault())
+  // lazy val currentZone = ZoneId.systemDefault()
   val halfayear: Long = ((31556952L / 2) * 1000)
   val recentLimit = System.currentTimeMillis - halfayear
-  //Instant.now.minusSeconds(31556952 / 2).toEpochMilli()
+  // Instant.now.minusSeconds(31556952 / 2).toEpochMilli()
 
   def date: Decorator
 
@@ -190,9 +191,9 @@ trait Core {
 //
 //    import Console.RESET
 
-    //val output = Seq(IconDecorator & ColorDecorator, IndicatorDecorator).foldLeft(Decorated(0, "")){
+    // val output = Seq(IconDecorator & ColorDecorator, IndicatorDecorator).foldLeft(Decorated(0, "")){
     //  case (d, action) ⇒ d |+| action(file)
-    //}
+    // }
 
 //    val decorated = if (config.hyperlink) hyperlink(file.path.toUri.toURL.toString, file.name) else file.name
     val decorator: Decorator = {
@@ -260,8 +261,8 @@ trait Core {
         override def decorate(file: FileInfo, builder: StringBuilder): Int = {
           val group =
             try {
-              //val group = file.group
-              //principalCache.getOrElseUpdate(group, group.getName)
+              // val group = file.group
+              // principalCache.getOrElseUpdate(group, group.getName)
               file.group
             } catch {
               case e: IOException => "-"
