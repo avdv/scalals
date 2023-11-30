2c4f349c5b49b0001101686ed30f510839aa88e4
Claudio Bley <claudio.bley@gmail.com>
2023-11-30 22:07:31 +0100

    Update nixpkgs to nixos-23.11
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/41de143fda10e33be0f47eab2bfe08a50f234267' (2023-11-27)
      → 'github:NixOS/nixpkgs/50aa30a13c4ab5e7ba282da460a3e3d44e9d0eb3' (2023-11-29)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "scalals";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
