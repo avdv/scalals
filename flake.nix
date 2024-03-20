492547cd8485adc7c40ffb3dfdaf40e3b8ba032e
Scala Steward <scala_steward@virtuslab.com>
2024-03-20 14:43:32 +0000

    Update sbt-buildinfo to 0.12.0

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
               # read the first non-empty string from the VERSION file
               version = builtins.head (builtins.match "[ \n]*([^ \n]+).*" (builtins.readFile ./VERSION));
 
-              depsSha256 = "sha256-VEMNkqKjJUB9ZpoATQW5LXcEHqXl6e5YK1Yx0gkMBKE=";
+              depsSha256 = "sha256-E1JZfHn409FuW7KuU6rhIyMqE5gv0XqKqz2yAdQyChs=";
 
               src = ./.;
 
