98ea19807d01a485b059bf2120fe6e59301417f9
Claudio Bley <claudio.bley@gmail.com>
2024-03-21 16:34:54 +0100

    Add scala-java-time to native project
    - remove local java.time.Instant implementation

--- native/src/main/scala/javatime.scala
+++
@@ -1,7 +0,0 @@
-package java.time
-
-case class Instant(seconds: Long):
-  def toEpochMilli(): Long = seconds * 1000
-
-object Instant:
-  def ofEpochSecond(seconds: Long): Instant = Instant(seconds)
