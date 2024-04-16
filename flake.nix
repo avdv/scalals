b0a84231ceb070c1e4f3ede95b549d6d271619a9
Claudio Bley <claudio.bley@gmail.com>
2024-04-16 18:31:33 +0200

    Enable sbt-tpolecat release mode for nix package

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
               NIX_CFLAGS_COMPILE = pkgs.lib.optional (with pkgs.stdenv; isLinux && isx86_64) "-march=sandybridge";
 
               buildPhase = ''
-                sbt 'project scalalsNative' 'show nativeConfig' ninjaCompile ninja
+                sbt tpolecatReleaseMode 'project scalalsNative' 'show nativeConfig' ninjaCompile ninja
                 ninja -f native/target/scala-3.3.3/native/build.ninja
               '';
 
